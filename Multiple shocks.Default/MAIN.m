f252b1e85a0112be3afae47d84a99e3e406598a6
Galo Nuño <37472288+galonuno@users.noreply.github.com>
2018-09-17 17:45:10 +0200

    Add files via upload

--- Multiple shocks.Default/MAIN.m
+++ Multiple shocks.Default/MAIN.m
@@ -36,42 +36,88 @@ parameters.y_ss         = 1.0 ; % output at steady state
 parameters.r_bar_ss     = 0.04 ; % steady-state short rate
 parameters.delta        = parameters.r_bar_ss ; % coupon = r_ss so we have bonds at par
 
-%% 1 PERFECT FORESIGHT 
-parameters.mode = 'PF'; % Perfect Foresight
 % Number of Shocks
 N_r=   1;
 N_y=   1;
 parameters.N_r=N_r;
 parameters.N_y=N_y;
 
-%Risky Steady State Parameters - No RSS
-parameters.phi  = 0.00       ; % Poisson arrival rate
-
-% Default parameters           - No default
-parameters.defmodel ='logit'     ; % default model
-parameters.muprobit =10;
-parameters.penalty  = -inf;        % Autarky penalty
-
 % Update to a matrix of joint probabilities - discrete matrix of marginals
 % - sum of row and columns must add up to 1
 parameters.prob_mat=1; % [s*]
 
 % Output parameters
-parameters.y_0(1:N_y,1)   = 1; % [s*] initial value, output after a shock
-parameters.rho_y(1:N_y,1) = (1-0.95)*4;  % [s*] persistence output after shock - 0.95 in quarterly data
-
-% 1.1 Interest rate shock
-%Interest Rate Shock Parameters
- parameters.r_bar_0(1:N_r,1)   = 0.05           ; % [s*] initial value volatility of 0.048 quarterly
-parameters.rho_r_bar(1:N_r,1) = (1-0.95)*4      ; % [s*] persistence (in years)
-execute_codes
-close all; plotting; cd figures; save PF_R; figure(2);   orient landscape; saveas(gcf,'DSS','pdf'); saveas(gcf,'DSS','fig'); figure(3);  saveas(gcf,'PF_IR','pdf'); saveas(gcf,'PF_IR','fig');close all; cd ..
-parameters.r_bar_0(1:N_r,1)   = 0.04        ; % [s*] initial value 
-% % 1.2 Output shock
-%Interest Rate Shock Parameters
-parameters.y_0(1:N_y,1)   = 0.95; % [s*] initial value, output after a shock
-execute_codes
-close all; plotting; cd figures; save PF_Y; figure(3); saveas(gcf,'PF_Y','pdf');saveas(gcf,'PF_Y','fig');close all;  cd ..
-parameters.y_0(1:N_y,1)   = 1; % [s*] initial value, output after a shock
+parameters.y_0(1:N_y,1)       = 1;                 % [s*] initial value, output after a shock
+parameters.rho_y(1:N_y,1)     = (1-0.95)*4;        % [s*] persistence output after shock - 0.95 in quarterly data
+parameters.r_bar_0(1:N_r,1)   = 0.04           ;   % [s*] initial value volatility of 0.048 quarterly
+parameters.rho_r_bar(1:N_r,1) = (1-0.95)*4      ;  % [s*] persistence (in years)
 
 
+% %% 1 PERFECT FORESIGHT 
+% parameters.mode = 'PF'; % Perfect Foresight
+% 
+% %Risky Steady State Parameters - No RSS
+% parameters.phi  = 0.00       ; % Poisson arrival rate
+% 
+% % Default parameters           - No default
+% parameters.defmodel ='logit'     ; % default model
+% parameters.muprobit =10;
+% parameters.penalty  = 1;        % Autarky penalty
+% 
+% % 1.1 Output shock
+% parameters.y_0(1:N_y,1)   = 0.95; % [s*] initial value, output after a shock
+% execute_codes % % 
+% close all; 
+% cd figures; 
+% save PF_Y; %figure(3); saveas(gcf,'PF_Y','pdf');saveas(gcf,'PF_Y','fig');close all;  
+% cd ..
+% parameters.y_0(1:N_y,1)   = 1; % [s*] initial value, output after a shock
+% 
+% % 1.2 Interest rate shock
+% parameters.r_bar_0(1:N_r,1)   = 0.05           ; % [s*] initial value
+% execute_codes;
+% close all;
+% cd figures; 
+% save PF_R; %figure(2);   orient landscape; saveas(gcf,'DSS','pdf'); saveas(gcf,'DSS','fig'); figure(3);  saveas(gcf,'PF_IR','pdf'); saveas(gcf,'PF_IR','fig');close all;
+% cd ..
+% parameters.r_bar_0(1:N_r,1)   = 0.04        ; % [s*] initial value 
+% 
+
+%% 2 RSS
+parameters.mode = 'RS'; % RSS
+
+%Risky Steady State Parameters - No RSS
+parameters.phi  = 0.02          ; % Poisson arrival rate
+
+% Default parameters           - No default
+parameters.defmodel ='logit'     ; % default model
+parameters.muprobit =10;
+parameters.penalty  = 1;        % Autarky penalty
+
+% %2.1 Output shock
+% parameters.y_0(1:N_y,1)   = 0.95; % [s*] initial value, output after a shock
+% execute_codes % % 
+% cd figures; save RSS_Y; cd ..
+% close all; 
+% parameters.y_0(1:N_y,1)   = 1; % [s*] initial value, output after a shock
+% 
+% 
+% % 2.2a Interest rate shock
+% parameters.r_bar_0(1:N_r,1)   = 0.05           ; % [s*] initial value volatility of 0.048 quarterly
+% execute_codes;
+% cd figures; save RSS_R; cd ..
+% close all; 
+% parameters.r_bar_0(1:N_r,1)   = 0.04        ; % [s*] initial value 
+% 
+% % 2.2a Interest rate shock
+parameters.r_bar_0(1:N_r,1)   = 0.05           ; % [s*] initial value volatility of 0.048 quarterly
+execute_codes;
+cd figures; save RSS_R; cd ..
+close all;
+parameters.r_bar_0(1:N_r,1)   = 0.04        ; % [s*] initial value
+
+
+
+%% PLOTTING
+% plotting_RSS
+plotting
